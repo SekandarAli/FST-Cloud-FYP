99c8eab3fc1f5d0a26d3c0b2bce2e0e6fee12c04
meassadkhan <85921482+meassadkhan@users.noreply.github.com>
2021-08-22 10:21:22 +0500

    vendor data adding + fetching + searching working

---
+++ app/src/main/java/com/example/Search/Search_data.kt
@@ -0,0 +1,77 @@
+package com.example.Search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fstsignin.R
+import com.example.fstsignin.Vendor.Vendor_Model
+import com.example.fstsignin.Vendor.showdata_adapter
+import com.google.firebase.database.*
+
+class Search_data : AppCompatActivity() {
+    private lateinit var dbref : DatabaseReference
+    private lateinit var DishRecycleview : RecyclerView
+    private lateinit var DishArrayList : ArrayList<Vendor_Model>
+    private lateinit var search_text : String
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_data)
+
+        DishRecycleview = findViewById(R.id.searchData_recycleview)
+        DishRecycleview.layoutManager = LinearLayoutManager(this)
+        DishRecycleview.setHasFixedSize(true)
+
+        DishArrayList = arrayListOf<Vendor_Model>()
+
+        var tv_search_text = findViewById<EditText>(R.id.ed_search_text)
+
+
+
+        var btn_search = findViewById<Button>(R.id.btn_search)
+        var btn_clear = findViewById<Button>(R.id.btn_clear).setOnClickListener {}
+
+        btn_search.setOnClickListener {
+            search_text = tv_search_text.text.toString()
+            Toast.makeText(this, "text = "+search_text, Toast.LENGTH_SHORT).show()
+            getData()}
+
+    }
+
+    private fun getData() {
+
+
+
+        dbref = FirebaseDatabase.getInstance().getReference()
+        var query : Query = dbref.child("Dish").orderByChild("dish_name").equalTo(search_text)
+
+        query.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for(userSnapShot in snapshot.children)
+                    {
+                        val dish = userSnapShot.getValue(Vendor_Model::class.java)
+
+                        DishArrayList.add(dish!!)
+                    }
+                    DishRecycleview.adapter = showdata_adapter(this@Search_data,DishArrayList)
+
+                }
+                else
+                {
+                    Toast.makeText(this@Search_data, "snapshot does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@Search_data, "Something went wrong", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+
+    }
+}
